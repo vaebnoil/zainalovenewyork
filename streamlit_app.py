ca855489143b22ae3d5fce9c5b0eca2119e686e2
vaebnoil <zainaavaleriee@gmail.com>
2025-05-27 09:23:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("Selamat Datang di Web New York Informatika")
 st.write(
     "ngoding seru bersama Zaina Valerie"
 )
-st.image ("0c934f12-60da-4204-9bd5-61536b770ce1.jpeg")
+st.image("0c934f12-60da-4204-9bd5-61536b770ce1.jpeg")
